d8920611d1ed966a07e45f49e1076ebac1849ea5
W0jti <wiechecwojciech@gmail.com>
2024-02-04 14:28:08 +0100

    Wojtek changes

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.hangman;
 
+import java.util.Arrays;
+
 public class Hangman {
 
     public static final String[] HANGMANPICS = new String[]{
@@ -55,5 +57,20 @@ public class Hangman {
     };
 
     public static void main(String[] args) {
+        for (int i = 0; i < HANGMANPICS.length; i++){
+            System.out.println(HANGMANPICS[i]);
+        }
+
+        String test = "Jacek";
+        char[] testArray = test.toCharArray();
+
+        for (int i = 0; i < testArray.length; i++){
+            System.out.print(testArray[i]);
+        }
+
+        
+        for (int i = 0; i < testArray.length; i++){
+            System.out.print(testArray[i]);
+        }
     }
 }
