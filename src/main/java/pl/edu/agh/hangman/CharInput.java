183b15dd5b1f47d6d2bb427d7d505a51899620aa
rysz <konrad.michal.rysz@gmail.com>
2024-02-04 14:43:16 +0100

    commit Konrad

--- src/main/java/pl/edu/agh/hangman/CharInput.java
+++ src/main/java/pl/edu/agh/hangman/CharInput.java
@@ -1,4 +1,15 @@
 package pl.edu.agh.hangman;
 
+import java.util.Scanner;
+
 public class CharInput {
+    public static void charInput(String[] args) {
+    }
+
+   static String enterLetter() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Wprowadź literę: ");
+        return input.next();
+    }
+
 }
